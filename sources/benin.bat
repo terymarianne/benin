7c9dcdacfbf8ca19472418e7b54931b415440761
Secrétariat Benin <secretariat.benin@gmail.com>
2021-11-28 18:13:27 +0100

    modification de l'executable

--- sources/benin.bat
+++ sources/benin.bat
@@ -1,4 +1,4 @@
 robocopy /e /PURGE  C:\benin C:\Users\Secretariat2\OneDrive\SauvegardeCarteBenin\Benin
 robocopy /e /PURGE  C:\carte_pdf C:\Users\Secretariat2\OneDrive\SauvegardeCarteBenin\carte_pdf
 
-python benin.py
+C:\Python\Python310\python.exe benin.py
