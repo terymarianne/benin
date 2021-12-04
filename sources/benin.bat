6aab54d1653ef5dde2a4cfd67fbce3519478e395
Secrétariat Benin <secretariat.benin@gmail.com>
2021-12-04 11:55:16 +0100

    suite à installation

--- sources/benin.bat
+++ sources/benin.bat
@@ -1,4 +1,4 @@
-robocopy /e /PURGE  C:\benin C:\Users\Secretariat2\OneDrive\SauvegardeCarteBenin\Benin
-robocopy /e /PURGE  C:\carte_pdf C:\Users\Secretariat2\OneDrive\SauvegardeCarteBenin\carte_pdf
+REM robocopy /e /PURGE  C:\benin C:\Users\Secretariat2\OneDrive\SauvegardeCarteBenin\Benin
+REM robocopy /e /PURGE  C:\carte_pdf C:\Users\Secretariat2\OneDrive\SauvegardeCarteBenin\carte_pdf
 
 C:\Python\Python310\python.exe benin.py
--- sources/scanconvert.bat
+++ sources/scanconvert.bat
@@ -1,3 +1,3 @@
-"c:\Program Files (x86)\IrfanView\i_view32.exe" /scanhidden /convert=c:\benin\scan\test.jpg
+"C:\Program Files (x86)\IrfanView\i_view32.exe" /scanhidden /convert=c:\benin\scan\test.jpg
 REM "c:\Program Files (x86)\IrfanView\i_view32.exe" c:\test\test.jpg /crop=(10,10,300,300) /convert=c:\test\testR.jpeg
-"c:\Program Files (x86)\IrfanView\i_view32.exe" c:\benin\scan\test.jpg /crop=(0,0,120,120) /convert=c:\benin\scan\testR.jpeg
+"C:\Program Files (x86)\IrfanView\i_view32.exe" c:\benin\scan\test.jpg /crop=(0,0,120,120) /convert=c:\benin\scan\testR.jpeg
